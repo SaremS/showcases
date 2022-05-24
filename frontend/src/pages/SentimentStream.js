fe0f09a8f5d7a92fb703710be6d6abef21fd89c3
Sarem Seitz <sarem.seitz@gmail.com>
2022-05-24 11:01:04 +0200

    update

--- frontend/src/pages/SentimentStream.js
+++ frontend/src/pages/SentimentStream.js
@@ -31,16 +31,10 @@ export default function SentimentStream({state,updateState}){
 				</Grid>	
 				<Grid item xs = {12} style={{marginRight:"60px"}}>
 					<h2>What does it do?</h2>
-					<p>Technically, the model detects random changes in the statistical distribution of the time-series. Contrary, to "deep" methods, we don't need a lot of data to produce reasonable results. In addition, the model learns without any annotated labels (= unsupervised) and can do so quite efficiently. The preset dataset shows TSLA stock returns from 13/05/2017 - 13/05/2022 and corresponing anomaly score. (large "blue" values indicate high probability of anomaly)</p> 
-					<p> Technically, we could also forecast the risk of anomalies at a future point in time. This is, however, not implemented yet.</p>
 <hr style={{opacity: 0.5, marginBottom: "30px"}}/>
 					<h2>How does it work?</h2>
-					<p>The model is built in <a href="https://www.julialang.com" target="_blank">Julia</a> and runs as a webservice using <a href="https://github.com/JuliaWeb/HTTP.jl" target="_blank">HTTP.jl.</a></p>
 <hr style={{opacity: 0.5, marginBottom: "30px"}}/>
 				<h2>How do I use it?</h2>
-				<p>Just upload your time-series as a .csv file by clicking the <b>Upload</b> button. The file should have a single column where each observation is stored in a new row (exporting a single-column Excel sheet as a .csv file should give you the correct format). The data is then sent to the model server which fits the model on your data and returns the probability of each observation being an anomaly. </p>
-				<p><b>The data is only used for model training and anomaly detection - no data gets stored anywhere</b></p>
-	<p>Unless the model server has to handle many requests in parallel, you should be able to get quick results if your dataset has a low 5-digit number of observations.</p>
 		</Grid>
 
 
