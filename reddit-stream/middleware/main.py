6ff754040bb66c4f0f391af3868dc9e36dcb788e
Sarem Seitz <sarem.seitz@gmail.com>
2022-05-27 14:48:56 +0200

    update

--- reddit-stream/middleware/main.py
+++ reddit-stream/middleware/main.py
@@ -4,8 +4,8 @@ import json
 from kafka import KafkaConsumer
 import os
 from collections import deque
-from copy import deepcopy
 import threading
+from functools import reduce
 
 if "KAFKA_HOST" in os.environ:
     KAFKA_HOST = os.getenv("KAFKA_HOST")
@@ -25,7 +25,6 @@ class DataCache():
         self._cache = deque(maxlen=500)
         self._full_topic = KAFKA_TOPIC + "_{}".format(target_topic)
         self._load_cache()
-        self._update_read = False
 
         self._consumer = KafkaConsumer(
                 self._full_topic,
@@ -49,7 +48,6 @@ class DataCache():
             data = json.loads(message.value.decode("utf-8"))
             self._cache.append(data)
             self._save_cache()
-            self._update_read = False
 
 
     def _load_cache(self):
@@ -72,8 +70,76 @@ class DataCache():
 
 
 
+class TitleCache():
+
+    def __init__(self, target_topic: str):
+        self._cache = []
+        self._full_topic = KAFKA_TOPIC + "_{}".format(target_topic)
+        self._load_cache()
+
+        self._consumer = KafkaConsumer(
+                self._full_topic,
+                bootstrap_servers=KAFKA_HOST
+        )
+        self.start_kafka_loop()
+
+
+    async def read(self):
+        min_one = self._get_min_from_cache("sentiment")
+        max_one = self._get_min_from_cache("sentiment")
+        return {"min": min_one, "max": max_one}
+   
+
+    def start_kafka_loop(self):
+        self._kafka_thread = threading.Thread(target=self._kafka_consumer_loop)
+        self._kafka_thread.start()
+
+
+    def _kafka_consumer_loop(self):
+        for message in self._consumer:
+            data = json.loads(message.value.decode("utf-8"))
+            
+            self._cache = list(filter(lambda x: x["timestamp"] > data["timestamp"] - 600, self._cache))
+            
+            self._cache.append(data)
+            self._save_cache()
+
+
+    def _get_max_from_cache(self, field: str = "timestamp"):
+        if len(self._cache)==0:
+            return None
+        else:
+            return reduce(lambda l,r: l if l[field]>r[field] else r, self._cache)
+
+
+    def _get_min_from_cache(self, field: str = "timestamp"):
+        if len(self._cache)==0:
+            return None
+        else:
+            return reduce(lambda l,r: l if l[field]<r[field] else r, self._cache)
+
+
+    def _load_cache(self):
+        try:
+            with open(self._full_topic + ".json","r") as f:
+                cache = json.load(f)
+                for item in cache:
+                    self._cache.append(item)
+        except Exception as e:
+            print(e)
+
+
+    def _save_cache(self):
+        with open(self._full_topic + ".json", 'w') as f:
+            json.dump(self.get_cache_as_list(), f)
+
+
+    def get_cache_as_list(self):
+        return list(self._cache)
+
+
 sentiment_mean_cache = DataCache("sentimented")
-title_sentiment_cache = DataCache("aggregated")
+title_sentiment_cache = TitleCache("aggregated")
 
 
 async def kafka_socket(websocket):
